bfe915975573572e6d31fd81d4c9d9d16ddfb2dd
DoublEffe <fabiofagiani9@gmail.com>
2024-05-08 18:41:20 +0200

    readme

---
+++ README.md
@@ -0,0 +1,122 @@
+<a name="readme-top"></a>
+
+
+<div align="center">
+  
+
+  <h3 align="center">Life Index Quality</h3>
+
+  <p align="center">
+    A place when you can search BLI (Better Life Index) by regions or city. 
+    <br />
+    <a href="https://github.com/DoublEffe/life-quality"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://life-quality-index.web.app/">View Demo</a>
+    ·
+    <a href="https://github.com/DoublEffe/life-quality/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/DoublEffe/life-quality/issues">Request Feature</a>
+  </p>
+</div>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+    </li>
+    <li>
+      <a href="#usage">Usage</a>
+      <ul>
+        <li><a href="#map">Map</a></li>
+        <li><a href="#Search Bar">Search Bar</a></li>
+      </ul>
+    </li> 
+    <li><a href="#license">License</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+![LifeIndex Screen Shot]()
+
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+### Built With
+
+This web page use only javascript.
+
+* [![javascript][javascript]][javascript-url]
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+There isn't much to do jut open the page.
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+A you can se the page is divided in two section the map and the search bar.
+
+### Map
+
+![Map Screen Shot]()
+
+In this map you can just click on a region to see data about people reading books: 
+  - People above 6 years old that read at least one book per year;
+  
+If you are seeing n.d this means that data are unavailable.
+(all data are gathered from Istat)
+
+### Search Bar
+
+![Search Bar Screen Shot]()
+
+In this serach bar you can search a book category and see all books in that category, and for aech book show title and authors.
+
+## Book Description
+
+![Search Result]()
+
+Each book can be clicked to show description and a graph that indicates in how many different categories the book appear.
+  
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!--variables-->
+[javascript]: https://img.shields.io/badge/Javascript-grey?style=for-the-badge&logo=javascript
+[javascript-url]: https://www.javascript.com/
--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,4 +1,5 @@
-import {regioni, keys} from './data.js'
+
+import {Regions} from './data.js'
 
 
 
@@ -23,12 +24,20 @@ async function bookDescriptionApi(key) {
   return {desc, numberOfCategory}
 }
 
-function createElement(elem) {
-  return document.createElement(elem)
-}
 
-function createElementN(s, elem) {
-  return document.createElementNS(s, elem)
+function createElementN(elem, attributeObj, appendTo) {
+  let el 
+  if (elem === 'svg' || elem === 'g' || elem === 'path') {
+    el = document.createElementNS('http://www.w3.org/2000/svg', elem)
+  }
+  else {
+    el = document.createElement(elem)
+  }
+  for(const [key, value] of Object.entries(attributeObj)) {
+    el.setAttribute(key, value)
+  }
+  appendTo.appendChild(el)
+  return el
 }
 
 async function istat(name){
@@ -44,31 +53,33 @@ async function istat(name){
 }
 
 //div that contains everything
-let mainDiv = createElement('div')
-document.body.appendChild(mainDiv)
-mainDiv.setAttribute('class', 'main')
+let body = document.getElementsByTagName('body')[0]
+let mainDiv = createElementN('div', {'class': 'main'}, body)
+
 
 //map of italy
-let map = createElementN('http://www.w3.org/2000/svg','svg')
-map.setAttribute('version', '1.1')
-map.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
-map.setAttribute('x', '0px')
-map.setAttribute('y', '0px')
-map.setAttribute('class', 'map')
-map.setAttribute('viewBox', '0 0 560.512 663.114')
-map.setAttribute('xml:space', 'preserve')
-let g = createElementN('http://www.w3.org/2000/svg','g')
-let defs = createElement('defs')
-map.appendChild(defs)
-map.appendChild(g)
+let obj = {}
+obj = {'version': '1.1', 
+      'xmlns': 'http://www.w3.org/2000/svg',
+      'x': '0px',
+      'y': '0px',
+      'class': 'map',
+      'viewBox': '0 0 560.512 663.114',
+      'xml:space': 'preserve'
+    }
+let map = createElementN('svg', obj, mainDiv)
+let g = createElementN('g', {}, map)
+let defs = createElementN('defs', {}, map)
+let regions = new Regions()
 for (let i=0; i < 20;i++){
-  let path = createElementN('http://www.w3.org/2000/svg','path')
-  //path.setAttribute('fill','#3E5C73')
-  path.setAttribute('class','regione')
-  path.setAttribute('data-nome-regione',keys[i])
-  path.setAttribute('data-code-regione',regioni[keys[i]].code)
-  path.setAttribute('d',regioni[keys[i]].path)
-  g.appendChild(path)
+  obj = {
+    'fill': '#3E5C73',
+    'class': 'regione',
+    'data-nome-regione': regions.getNames(i),
+    'data-code-regione': regions.getCode(i),
+    'd': regions.getPath(i)
+  }
+  let path = createElementN('path', obj, g)
   path.onclick = async () => {
     let paths = document.querySelectorAll('path')
     paths.forEach((path) => {
@@ -86,31 +97,20 @@ for (let i=0; i < 20;i++){
     trigger: 'click',
     async onTrigger(){
       let arr = await istat(path.dataset.codeRegione)
-      tooltip.setContent(`<p><bold>${path.dataset.nomeRegione}</bold></p><p>people reading a book from 6 years old: ${arr[0]}</p>`)
+      tooltip.setContent(`<p><bold>${path.dataset.nomeRegione}</bold></p><p>people reading a book above 6 years old: ${arr[0]}</p>`)
     }
   })
 }
-mainDiv.appendChild(map)
+
 
 //search bar
-let formDiv = createElement('div')
-let form = createElement('form')
-let searchDiv = createElement('div')
-let searchBox = createElement('input')
-let searchButton = createElement('button')
-let infoDiv = createElement('div')
-infoDiv.setAttribute('id', 'info')
-formDiv.setAttribute('class', 'form-div')
-searchDiv.setAttribute('class', 'search-div')
-formDiv.appendChild(searchDiv)
-formDiv.appendChild(infoDiv)
-searchDiv.appendChild(form)
-form.setAttribute('id', '#form')
-form.appendChild(searchBox)
-form.appendChild(searchButton)
+let formDiv = createElementN('div', {'class': 'form-div'}, mainDiv)
+let searchDiv = createElementN('div', {'class': 'search-div'}, formDiv)
+let form = createElementN('form', {'id': '#form'}, searchDiv)
+let searchBox = createElementN('input', {'name': 'search-bar'}, form)
+let searchButton = createElementN('button', {'type': 'submit'}, form)
+let infoDiv = createElementN('div', {'id': 'info'}, formDiv)
 searchButton.textContent = 'Search'
-searchButton.setAttribute('type', 'submit')
-searchBox.setAttribute('name', 'search-bar')
 //remove the infbox with new search
 searchBox.onfocus = () => {
   let infoDiv = document.getElementById('info')
@@ -131,29 +131,21 @@ form.onsubmit = async (e) => {
 searchBox.onfocusout = () => {
   formDiv.appendChild(infoDiv)
 }
-mainDiv.appendChild(formDiv)
+
 
 //information after the search
 const bookBox = (works) => {
-  let bookDiv = createElement('div')
-  bookDiv.setAttribute('id', 'info')
-  formDiv.appendChild(bookDiv)
+  let bookDiv = createElementN('div', {'id': 'info'}, formDiv)
   //create a paragraph with title and authors element for each book
   works.forEach(book => {
-    let title = createElement('p')
-    title.setAttribute('class', 'book-title')
+    let title = createElementN('p', {'class': 'book-title'}, bookDiv)
     title.innerText = 'Book Title: ' + book.title.toUpperCase()
-    let p = createElement('p')
-    p.setAttribute('class', 'book-info')
+    let p = createElementN('p', {'class': 'book-info'}, title)
     p.innerText = 'Authors: '
     book.authors.forEach(author => {
-      let authorText = createElement('p')
+      let authorText = createElementN('p', {'class': 'authors'}, p)
       authorText.innerText = author.name
-      authorText.style.alignSelf = 'center'
-      p.appendChild(authorText)
     })
-    title.appendChild(p)
-    bookDiv.appendChild(title)
     //showing the description for each book
     title.addEventListener('click', () => descBox(book.key, title))
   }) 
@@ -161,15 +153,12 @@ const bookBox = (works) => {
 
 //description and canva that appear after clicking a book
 const descBox = async (key, title) => {
-  let descDiv = createElement('div')
-  let graph = createElement('canvas')
-  let p = createElement('p')
-  descDiv.setAttribute('class', 'desc-div')
-  graph.setAttribute('class', 'graph')
+  let descDiv = createElementN('div', {'class': 'desc-div'}, title)
+  let p = createElementN('p', {}, descDiv)
+  let graph = createElementN('canvas', {'class': 'graph'}, descDiv)
   try{
     let {desc, numberOfCategory} = await bookDescriptionApi(key)
     p.innerText = desc
-    
     new Chart(graph, {
       type: 'bar',
       data: {
@@ -220,13 +209,8 @@ const descBox = async (key, title) => {
         }
       }
     });
-    descDiv.appendChild(p)
-    descDiv.appendChild(graph)
   }
   catch (e){
      alert('Error: '+ e)
-  }
-  
-  title.appendChild(descDiv)
-  
+  } 
 }
